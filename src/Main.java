253cf3c708d076d94e47f2e163db73824e244ef0
shintaayuck <shinta990405@gmail.com>
2019-04-22 21:31:23 +0700

    [ADD] CRDT, VersionVector and Character Model Classes
    local insert and delete already tested (except broadcasting)
    remote insert and delete implemented, not yet tested

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
-public class Main {
+import model.CRDT;
+import model.Version;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+
+public class Main{
     public static void main(String[] args) {
-        
+        testCRDT();
+    }
+    
+    public static void testCRDT() {
+    
+        HashMap<Integer, Version> versions = new HashMap<>();
+        CRDT crdt = new CRDT(1, versions);
+        crdt.localInsert(0f, 'q');
+        crdt.localInsert(0f, 'a');
+        crdt.localInsert(Float.MAX_VALUE, 'u');
+        crdt.localInsert(Float.MAX_VALUE, 'e');
+        crdt.localInsert(-1f, '!');
+        crdt.localInsert(1f,'@');
+        crdt.localInsert(1f,'$');
+        crdt.localInsert(1f,'$');
+        crdt.localInsert(0.875f,'x');
+        crdt.localInsert(1f,'p');
+    
+        crdt.localDelete(1f);
+
+
+//        SortedMap<Float, Character> chars = new TreeMap<>();
+//
+//        chars.put(-1.4f, 'q');
+//        chars.put(1.45f, 's');
+//        chars.put(1.451f, 'k');
+//        chars.put(1.351f, 'x');
+////
+//        System.out.println(chars);
+//
+//
+//        System.out.println(chars.lastKey());
+//
+////        System.out.println(((TreeMap<Float, Character>) chars).higherEntry(1.35f));
+//
+//        System.out.println(35%1);
+//        System.out.println((35 %1 == 0 ? 35+1 : (float) ceil(35)));
+//
+//        ArrayList<Integer> ints = new ArrayList<>();
+//        ints.add(10);
+//        ints.add(11);
+//        ints.add(9);
+//        System.out.println(ints);
+//        ints.set(1, ints.get(1)+10);
+//        System.out.println(ints);
+    
     }
 }
--- src/model/CRDT.java
+++ src/model/CRDT.java
@@ -1,57 +1,107 @@
 package model;
 
+import java.util.HashMap;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.floor;
+
 public class CRDT {
-    private String idSite;
-    private Boolean isInsert;
-    private Character value;
-    private Float[] positions;
-    private Integer counter;
+    public static SortedMap<Float, Character> document = new TreeMap<>();
+    public static int counter = 0;
+    public int idNode;
+    public HashMap<Integer, Version> versions;
     
-    public CRDT(String idSite, Boolean isInsert, Character value, Float[] positions, Integer counter) {
-        this.idSite = idSite;
-        this.isInsert = isInsert;
-        this.value = value;
-        this.positions = positions;
-        this.counter = counter;
+    public CRDT(int idNode, HashMap<Integer, Version> versions) {
+        this.idNode = idNode;
+        this.versions = versions;
     }
     
-    public String getIdSite() {
-        return idSite;
+    public int getIdNode() {
+        return idNode;
     }
     
-    public void setIdSite(String idSite) {
-        this.idSite = idSite;
+    public void localInsert(Float nextIdx, char value) {
+        Float position = setCharPosition(nextIdx);
+        Character c = new Character(value, position, new Version(this.getIdNode(), counter++));
+        document.put(position, c);
+        broadcastChar(c);
+        
     }
     
-    public Boolean getInsert() {
-        return isInsert;
+    private Float setCharPosition(Float nextIdx) {
+        if (document.size() == 0) {
+            return nextIdx;
+        }
+        else if (nextIdx == Float.MAX_VALUE) {
+            return (float) ceil(document.lastKey()+1);
+        } else if (nextIdx.equals(document.firstKey())) {
+            return (float) floor(document.firstKey()-1);
+//            return ((document.firstKey() % 1 == 0) ? document.firstKey()-1 : (float) floor(document.firstKey()));
+        } else {
+          Float low = ((TreeMap<Float,Character>)document).lowerKey(nextIdx) != null ?
+              ((TreeMap<Float,Character>)document).lowerKey(nextIdx) : 0f;
+          return (low + nextIdx) / 2;
+        }
+
     }
     
-    public void setInsert(Boolean insert) {
-        isInsert = insert;
+    public void localDelete(Float idx) {
+        Character c = document.remove(idx);
+        Version version = new Version(idNode, counter++);
+        c.delete(version);
+        broadcastChar(c);
     }
     
-    public Character getValue() {
-        return value;
+    private void broadcastChar(Character c) {
+        Character[] cs = new Character[0];
+        cs = document.values().toArray(cs);
+        for (Character c1:
+             cs) {
+            System.out.print(c1.getValue());
+        }
+        System.out.println("");
+        for (Character c1:
+            cs) {
+            System.out.print(c1.getPosition() + " ");
+        }
+        System.out.println("");
+        for (Character c1:
+            cs) {
+            System.out.print(c1.getInsertVersion().getCounter() + " ");
+        }
+    
+        System.out.println("");
+        System.out.println("");
+        
+        if (!c.getInsert()) {
+            System.out.println("DELETE " + c.getDeleteVersion().getCounter());
+        }
     }
     
-    public void setValue(Character value) {
-        this.value = value;
+    public void remoteInsert(Character c) {
+        document.put(c.getPosition(),c);
+        Version version = versions.get(c.getInsertVersion());
+        version.setCounter(
+            c.getInsertVersion().getCounter() < version.getCounter() ?
+                version.getCounter() : c.getInsertVersion().getCounter());
+        broadcastChar(c);
     }
     
-    public Float[] getPositions() {
-        return positions;
-    }
-    
-    public void setPositions(Float[] positions) {
-        this.positions = positions;
-    }
-    
-    public Integer getCounter() {
-        return counter;
-    }
-    
-    public void setCounter(Integer counter) {
-        this.counter = counter;
+    public void remoteDelete(Character c) {
+        
+        if (c.getInsertVersion().getIdNode() != this.idNode &&
+            versions.get(c.getInsertVersion().getIdNode()).getCounter() < c.getInsertVersion().getCounter())  {
+                //Add Deletion Buffer
+        } else {
+            document.remove(c.getPosition());
+            Version version = versions.get(c.getDeleteVersion().getIdNode());
+            version.setCounter(
+                version.getCounter() < c.getDeleteVersion().getCounter() ?
+                    c.getDeleteVersion().getCounter() : version.getCounter()
+            );
+        }
+        broadcastChar(c);
     }
 }
